9fd5a92a7934903c81f6f5153443d86291f1d99c
btLong402 <111285054+btLong402@users.noreply.github.com>
2023-12-18 09:15:42 +0700

    function create group & upload file

--- src/Client.java
+++ src/Client.java
@@ -1,49 +1,82 @@
-import java.io.*; 
-import java.net.*; 
-import java.util.*; 
-
+import java.io.*;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import javax.imageio.ImageIO;
 // Client class 
-class Client { 
-	
-	// driver code 
-	public static void main(String[] args) 
-	{ 
-		// establish a connection by providing host and port 
-		// number 
-		try (Socket socket = new Socket("localhost", 5555)) { 
-			
-			// writing to server 
-			PrintWriter out = new PrintWriter( 
-				socket.getOutputStream(), true); 
+class Client {
 
-			// reading from server 
-			BufferedReader in 
-				= new BufferedReader(new InputStreamReader( 
-					socket.getInputStream())); 
+	// driver code
+	public static void main(String[] args) {
+		// establish a connection by providing host and port
+		// number
 
-			// object of scanner class 
-			Scanner sc = new Scanner(System.in); 
-			String line = null; 
+		try (Socket socket = new Socket("localhost", 5555)) {
 
-			while (!"exit".equalsIgnoreCase(line)) { 
-				
-				// reading from user 
-				line = sc.nextLine(); 
+			// writing to server
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-				// sending the user input to server 
-				out.println(line); 
-				out.flush(); 
+			// reading from server
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			while (true) {
+				BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+				String cmd = sc.readLine();
+				if (cmd.equals("Exit"))
+					break;
+				StringTokenizer command = new StringTokenizer(cmd, " ");
+				String key = command.nextToken().toUpperCase();
+				switch (key) {
+				case "CREATE_GROUP":
+//					String userName = command.nextToken();
+//					String groupName = command.nextToken();
+					out.writeUTF(cmd);
+	                out.flush();
+					break;
+				case "UPLOAD_FILE":
+					String groupName = command.nextToken();
+                    String filePath = command.nextToken();
+                    File fileSource = new File(filePath);
+                    
+                    if (!fileSource.exists()) {
+                        System.out.println("Source file name not exists!!!");
+                        break;
+                    }
+                    else {
+                    	 try {
+                             // Sử dụng Files.probeContentType() để lấy loại của file
+                             Path path = Paths.get(filePath);
+                             String fileType = Files.probeContentType(path);
 
-				// displaying server reply 
-				System.out.println("Server replied "
-								+ in.readLine()); 
-			} 
-			
-			// closing the scanner object 
-			sc.close(); 
-		} 
-		catch (IOException e) { 
-			e.printStackTrace(); 
-		} 
-	} 
+                             if (fileType != null) {
+                                 System.out.println("File Type: " + fileType);
+                             } else {
+                                 System.out.println("Unable to determine the file type.");
+                             }
+                         } catch (IOException e) {
+                             e.printStackTrace();
+                         }
+                    }
+                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileSource));
+//                    byte[] fileContent = Files.readAllBytes(Paths.get(filePath));
+                    out.writeUTF(cmd);
+                    out.writeLong(fileSource.length());
+                    int data;
+                    while ((data = bis.read()) != -1) {
+                        out.write(data);
+                        out.flush();
+                    }
+                    bis.close();
+					break;
+				default:
+					break;
+				}
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,14 @@
 import java.io.*; 
 import java.net.*; 
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
 // Server class 
 class Server { 
 	public static void main(String[] args) 
 	{ 
 		ServerSocket server = null; 
-//		 Path currentPath = Paths.get("");
-//	     System.out.println("Current Path: " + currentPath.toAbsolutePath().getParent());
+		 
 
 		try { 
 
@@ -57,37 +57,77 @@ class Server {
 	// ClientHandler class 
 	private static class ClientHandler implements Runnable { 
 		private final Socket clientSocket; 
-
+		DataInputStream in; 
+		DataOutputStream out; 
+		Path currentPath = Paths.get("").toAbsolutePath().getParent().resolve("root");
 		// Constructor 
-		public ClientHandler(Socket socket) 
+		public ClientHandler(Socket socket) throws IOException 
 		{ 
 			this.clientSocket = socket; 
+			in = new DataInputStream(socket.getInputStream()); 
+			
+			// get the inputstream of client 
+			out = new DataOutputStream(socket.getOutputStream()); 
 		} 
 
 		public void run() 
 		{ 
-			PrintWriter out = null; 
-			BufferedReader in = null; 
 			try { 
 					
 				// get the outputstream of client 
-				out = new PrintWriter( 
-					clientSocket.getOutputStream(), true); 
-
-				// get the inputstream of client 
-				in = new BufferedReader( 
-					new InputStreamReader( 
-						clientSocket.getInputStream())); 
 
 				String line; 
-				while ((line = in.readLine()) != null) { 
-
+				while ((line = in.readUTF()) != null) { 
+					System.out.println(line);
 					// writing the received message from 
 					// client 
-					System.out.printf( 
-						" Sent from the client: %s\n", 
-						line); 
-					out.println(line); 
+					StringTokenizer command = new StringTokenizer(line, " ");
+					String key = command.nextToken().toUpperCase();
+					switch (key) {
+					case "CREATE_GROUP":
+						String userName = command.nextToken();
+						String groupName = command.nextToken();
+						String path = currentPath.resolve(groupName).toString();
+//						System.out.println(path);
+						File folder = new File(path);
+
+				        // Kiểm tra xem thư mục đã tồn tại chưa, nếu không thì tạo mới
+				        if (!folder.exists()) {
+				            boolean result = folder.mkdirs(); // Tạo thư mục và tất cả các thư mục cha nếu chưa tồn tại
+				            if (result) {
+				                System.out.println("Create success!");
+				            } else {
+				                System.out.println("Can not create!");
+				            }
+				        } else {
+				            System.out.println("Group existed!");
+				        }
+						break;
+					case "UPLOAD_FILE":
+						String groupNameUpload = command.nextToken();
+						String filePath = command.nextToken();
+
+					    // Extract the file name from the file path
+					    String fileName = filePath.substring(filePath.lastIndexOf(File.separator) + 1);
+					    System.out.println(fileName);
+						long fileSize = in.readLong();
+						String destinationPath = currentPath.resolve(groupNameUpload).resolve(fileName).toString();
+						File f = new File(destinationPath);
+						BufferedOutputStream bos;
+						try {
+							bos = new BufferedOutputStream(new FileOutputStream(f));
+							for (int i = 0; i < fileSize; i++) {
+								bos.write(in.read());
+								bos.flush();
+							}
+							System.out.println("Upload successed!");
+							bos.close();
+						} catch (IOException e) {
+						}
+						break;
+					default:
+						break;
+					}
 				} 
 			} 
 			catch (IOException e) { 
