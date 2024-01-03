cdfa04edde1581835f9b02c7a312d22b9c5d8ed9
btLong402 <111285054+btLong402@users.noreply.github.com>
2024-01-03 20:57:42 +0700

    fix

--- src/helper/respone/payload/group/RemoveMemberPayload.java
+++
@@ -1,7 +0,0 @@
-package helper.respone.payload.group;
-
-import helper.respone.payload.BasePayload;
-
-public class RemoveMemberPayload extends BasePayload {
-
-}
---
+++ src/helper/response/_response/Response.java
@@ -0,0 +1,15 @@
+package helper.response._response;
+
+import helper.response.payload.BasePayload;
+
+public class Response {
+	
+	public int responseCode;
+	public BasePayload payload;
+
+	public Response(String messageType, int responseCode, BasePayload payload) {
+		super();
+		this.responseCode = responseCode;
+		this.payload = payload;
+	}
+}
